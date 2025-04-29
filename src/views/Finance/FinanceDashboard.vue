4e083902d4e72c489cc2be4e9f7391cd2b2af87b
natsdyy <charleslouiealvaran@gmail.com>
2025-04-29 14:40:30 +0800

    Message 1

--- src/views/Finance/FinanceDashboard.vue
+++ src/views/Finance/FinanceDashboard.vue
@@ -3,5 +3,12 @@
 <template>
   <div>
     <h1 class="text-2xl font-bold text-brownColor">This is Dashboard</h1>
+    <div class="flex flex-col md:flex-row">
+      <div class="w-full md:w-1/4">
+        <AdminSidebar />
+      </div>
+      <div class="w-full md:w-3/4">
+        <router-view />
+      </div>    
   </div>
 </template>
